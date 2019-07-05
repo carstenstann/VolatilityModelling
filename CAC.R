cb8d7d1f8de675d770fd538e95bda1871a2cdba7
Carsten Stann <carsten.stann@gmail.com>
2019-07-05 18:22:21 +0200

    update README

--- CAC.R
+++ CAC.R
@@ -1,43 +1,42 @@
-library(readr)
-library(dplyr)
-library(broom)
-library(tibble)
-library(xts)
-library(lubridate)
-library(FinTS)
-library(forecast)
-library(rugarch)
-library(tictoc)
+library(tidyverse)
+library(tsibble)
 
-## Import data and convert dates
-import <- as_tibble(read_csv("/Users/carsten 1/Desktop/Forecasting Paper/OxfordManRealizedVolatilityIndices.csv", 
-                             skip = 2))
-import$DateID <- as.character(import$DateID)
-import$DateID<- as_date(import$DateID, format = "%Y%m%d")
+# Import data ---------------------------------------------------------------------------
+path <- "/Users/Carsten/Documents/GitHub/VolatilityModelling/Data/OxfordManRealizedVolatilityIndices.csv"
 
-## Subset RV and Return data, convert to XTS objects
+import <- read_csv(path, skip = 2)
+
+glimpse(import)
+
+vol_data <- import %>%
+   mutate(DateID = parse_date(as.character(DateID), format = "%Y%m%d")) %>% 
+   select(DateID, contains(".r"), -contains(".rk")) %>% 
+   as_tsibble(index = DateID)
+
+vol_data
+
+# Subset RV and Return data, convert to XTS objects
 RV.na <- import %>% select(FCHI2.rv) %>% 
-  as.xts(order.by = import$DateID) ## FCHI2.rv -- SPX2.rv, GDAXI2.rv, FTSE2.rv, N2252.rv, DJI2.rv
+  as.xts(order.by = import$DateID) # FCHI2.rv -- SPX2.rv, GDAXI2.rv, FTSE2.rv, N2252.rv, DJI2.rv
 
 returns.na <- import %>% select(FCHI2.r) %>% 
-  as.xts(order.by = import$DateID) ## FCHI2.r -- SPX2.r, GDAXI2.r, FTSE2.r, N2252.r, DJI2.r
+  as.xts(order.by = import$DateID) # FCHI2.r -- SPX2.r, GDAXI2.r, FTSE2.r, N2252.r, DJI2.r
 
-## Interpolate NAs linearly
+# Interpolate NAs linearly
 RV <- na.approx(RV.na)
 returns <- na.approx(returns.na)
-## Clip first and last observation due to NAs
+# Clip first and last observation due to NAs
 RV<- RV[-1,]
 RV <- RV[-length(RV),]
 returns <- returns[-1,]
 returns <- returns[-length(returns),]
 
-## fix column names
-colnames(RV) <- c("CAC") ##"FTSE100", "Nikkei225", "DAX", "DJI", "CAC40"
-colnames(returns) <- c("CAC") ##"FTSE100", "Nikkei225", "DAX", "DJI", "CAC40"
+# fix column names
+colnames(RV) <- c("CAC") #"FTSE100", "Nikkei225", "DAX", "DJI", "CAC40"
+colnames(returns) <- c("CAC") #"FTSE100", "Nikkei225", "DAX", "DJI", "CAC40"
 
-
-## subset data for training and test sets (4 and 8 yrs Sept.1 2000/2004 - March1, 2009)
-## Test set is 2008-09-01 through 2009-09-01
+# subset data for training and test sets (4 and 8 yrs Sept.1 2000/2004 - March1, 2009)
+# Test set is 2008-09-01 through 2009-09-01
 start.8 <- as.Date("2000-09-01")
 start.4 <- as.Date("2004-09-01") 
 start.test <- as.Date("2008-09-01")
@@ -47,56 +46,56 @@ train.4 <-subset(returns, index(returns)>= start.4 & index(returns) < start.test
 returns.8 <- subset(returns, index(returns)>= start.8 & index(returns) < end)
 returns.4 <- subset(returns, index(returns)>= start.4 & index(returns) < end)
 
-## Subset Volatility Proxies
+# Subset Volatility Proxies
 RV.8 <- subset(RV, index(RV)>= start.8 & index(RV) < end)
 RV.4 <- subset(RV, index(RV)>= start.4 & index(RV) < end)
 crisis.rv <-subset(RV, index(RV) >= as.Date("2008-09-01") & index(RV) < end)
-crisis.r <-subset(returns, index(returns)>= start.test & index(returns) < end) ## subset of returns for test set
-s.crisis.r <- crisis.r^2 ## squared returns for test set
+crisis.r <-subset(returns, index(returns)>= start.test & index(returns) < end) # subset of returns for test set
+s.crisis.r <- crisis.r^2 # squared returns for test set
 
-## Plot data
+# Plot data
 plot.zoo(returns.8)
 plot.zoo(RV.8)
 plot.zoo(crisis.r)
 plot.zoo(crisis.rv)
 
-## Plot ACFS
-## ACF of returns
+# Plot ACFS
+# ACF of returns
 par(mfrow = c(3,2))
 Acf.returns <- Acf(returns.8) %>% tidy()
 
-## ACFs of squared returns
+# ACFs of squared returns
 Acf.returns <- Acf(returns.8^2, lag.max = 50) %>% tidy()
 
-## ACF returns during crisis
+# ACF returns during crisis
 Acf.returns.c <- Acf(crisis.r^2, lag.max = 50) %>% tidy()
 
-## reset plots
+# reset plots
 par(mfrow=c(1,1))
 
-## Fit AR 1 and simple mean. Simple Mean has higher AIC
+# Fit AR 1 and simple mean. Simple Mean has higher AIC
 fit1 <- Arima(train.8, order = c(1,0,0), include.mean = FALSE)
 fit2 <- Arima(train.8, order = c(0,0,0), include.mean = FALSE)
 fit1
 fit2
 
-## Test for ARCH effects: 1) Ljung-Box with Nul that m lags of 
-##                           ACF of squared resids are zero (AutocorTest) 
-##                        2) Lagrange Multiplier Test (engle 1982) (ArchTest)
-##                           See Tsay book 134 and Tsay (2005) 101-102
+# Test for ARCH effects: 1) Ljung-Box with Nul that m lags of 
+#                           ACF of squared resids are zero (AutocorTest) 
+#                        2) Lagrange Multiplier Test (engle 1982) (ArchTest)
+#                           See Tsay book 134 and Tsay (2005) 101-102
 AutocorTest(fit1$residuals^2, lag=12, type = "Ljung-Box")  
 ArchTest(fit2$residuals^2, lags=12, demean = FALSE)
 
 
-## Model selection: Which Model has Best In-Sample Fit?
-## Justify STD vs. Norm in distribution
+# Model selection: Which Model has Best In-Sample Fit?
+# Justify STD vs. Norm in distribution
 arch.order <- c(1:5)
 arch.names <- paste("arch", arch.order, sep ="")
 tarch.order <- c(1:5)
 tarch.names <- paste("tarch", arch.order, sep ="")
 model.count <- 1
 fit.list <- list()
-## Fit Arch(1:5)
+# Fit Arch(1:5)
 for(p in arch.order){
   arch.spec <- ugarchspec(variance.model = list(garchOrder = c(p,0)),
                           mean.model = list(armaOrder = c(0,0), include.mean = FALSE), 
@@ -105,7 +104,7 @@ for(p in arch.order){
   fit.list[[model.count]] <- arch.fit
   model.count <- model.count + 1
 }
-##Fit TARCH(1:5) TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
+#Fit TARCH(1:5) TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
 for(t in tarch.order){
   tarch.spec <- ugarchspec(variance.model = list(model = "fGARCH", garchOrder = c(t,0), 
                                                  submodel = "TGARCH"),
@@ -116,30 +115,30 @@ for(t in tarch.order){
   model.count <- model.count + 1
 }
 names(fit.list) <- c(arch.names, tarch.names)
-##fit GARCH11
+#fit GARCH11
 garch.spec<- ugarchspec(variance.model = list(garchOrder = c(1,1)),
                         mean.model = list(armaOrder = c(0,0), include.mean = FALSE),
                         distribution.model = "std")
 fit.list$garch11 <- ugarchfit(spec = garch.spec, data = train.8, solver = "hybrid")
 
-## fit Thr- GARCH11 TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
+# fit Thr- GARCH11 TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
 tgarch.spec<- ugarchspec(variance.model = list(model = "fGARCH", garchOrder = c(1,1), 
                                                submodel = "TGARCH"),
                          mean.model = list(armaOrder = c(0,0), include.mean = FALSE),
                          distribution.model = "std")
 fit.list$tgarch11<- ugarchfit(spec=tgarch.spec, data = train.8, solver = "hybrid")
 
-## collect model data into matrix
+# collect model data into matrix
 info.mat <- sapply(fit.list, infocriteria)
 rownames(info.mat)<- rownames(infocriteria(fit.list[[1]]))
 info.mat
 
 
-## demonstrate normailty is poor assumtion. Favor Student-t dist.
+# demonstrate normailty is poor assumtion. Favor Student-t dist.
 
-##ARCH model by Engle (1982)
-##GARCH11 Bollerslev (1986)
-##TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
+# ARCH model by Engle (1982)
+# GARCH11 Bollerslev (1986)
+# TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
 arch.order <- c(1:5)
 tarch.order <- c(1:5)
 roll <- c(5,10,15,20)
@@ -160,7 +159,7 @@ for(r in roll){
     roll.list[[model.count]] <- arch.roll
     model.count <- model.count+1
   }
-  ## TARCH 1-5 Zakoian (1994) found especially effective in Hansen Lunde 2006
+  # TARCH 1-5 Zakoian (1994) found especially effective in Hansen Lunde 2006
   for(t in tarch.order){
     tarch.spec <- ugarchspec(variance.model = list(model = "fGARCH", garchOrder = c(t,0), 
                                                    submodel = "TGARCH"),
@@ -175,7 +174,7 @@ for(r in roll){
     roll.list[[model.count]] <- tarch.roll
     model.count <- model.count + 1
   }
-  ## Garch (1,1) of Bollerslev (1986)
+  # Garch (1,1) of Bollerslev (1986)
   garch.spec<- ugarchspec(variance.model = list(garchOrder = c(1,1)),
                           mean.model = list(armaOrder = c(0,0), include.mean = FALSE),
                           distribution.model = "std")
@@ -185,7 +184,7 @@ for(r in roll){
                                          solver.control = list(), calculate.VaR = TRUE, VaR.alpha = c(0.01,0.05),
                                          cluster = NULL, keep.coef = TRUE)
   model.count<- model.count + 1
-  ## TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
+  # TGARCH Zakoian (1994) found especially effective in Hansen Lunde 2006
   tgarch.spec<- ugarchspec(variance.model = list(model = "fGARCH", garchOrder = c(1,1), 
                                                  submodel = "TGARCH"),
                            mean.model = list(armaOrder = c(0,0), include.mean = FALSE),
@@ -198,16 +197,18 @@ for(r in roll){
   model.count<- model.count + 1
 }
 toc()
-## Get Volatility Forecasts (Sigma is Conditional Standard Deviation! Square to get conditional variance)
+
+# Get Volatility Forecasts (Sigma is Conditional Standard Deviation! Square to get conditional variance)
 var.mat <- sapply(roll.list, function(x) x@forecast$density[,"Sigma"]^2)
 rv.crisis <- coredata(crisis.rv)
 sr.crisis <- coredata(s.crisis.r)
-## Calculate average MSE using both RV ans Squared Returns as volatility proxies for each model and rolling forecast
+
+# Calculate average MSE using both RV ans Squared Returns as volatility proxies for each model and rolling forecast
 mse.rv.mat <- apply(var.mat, MARGIN = 2, function(x) ((rv.crisis-x)^2))
 mse.sr.mat <- apply(var.mat, MARGIN = 2, function(x) ((sr.crisis-x)^2))
 mean.mse.rv.mat <- apply(mse.rv.mat, MARGIN = 2, mean)
 mean.mse.sr.mat <- apply(mse.sr.mat, MARGIN = 2, mean)
-mean.mse.rv.mat.1 <- matrix(mean.mse.rv.mat, nrow = 12, ncol = length(roll), byrow = FALSE)##ncol = number of rolls, nrow = # models
+mean.mse.rv.mat.1 <- matrix(mean.mse.rv.mat, nrow = 12, ncol = length(roll), byrow = FALSE)#ncol = number of rolls, nrow = # models
 mean.mse.sr.mat.1 <- matrix(mean.mse.sr.mat, nrow = 12, ncol = length(roll), byrow = FALSE)
 mse.final <- cbind(mean.mse.rv.mat.1, mean.mse.sr.mat.1)
 colnames(mse.final)<- c("one.week.rv", "two.week.rv", "three.week.rv", "four.week.rv",
@@ -216,12 +217,12 @@ rownames(mse.final)<- c("ARCH1", "ARCH2", "ARCH3", "ARCH4", "ARCH5",
                         "TARCH1", "TARCH2", "TARCH3", "TARCH4", "TARCH5", 
                         "GARCH11", "TGARCH11")
 
-## Calculate average QL using both RV ans Squared Returns as volatility proxies for each model and rolling forecast
+# Calculate average QL using both RV ans Squared Returns as volatility proxies for each model and rolling forecast
 
 ql.rv.mat <- apply(var.mat, MARGIN = 2,
-                   function(x) (rv.crisis/x-log(rv.crisis/x)-1) ) ## QLIKE FROM BROWNLESS pg.8#(log(x)+(rv.crisis/x))
+                   function(x) (rv.crisis/x-log(rv.crisis/x)-1) ) # QLIKE FROM BROWNLESS pg.8#(log(x)+(rv.crisis/x))
 ql.sr.mat <- apply(var.mat, MARGIN = 2,
-                   function(x) (sr.crisis/x-log(sr.crisis/x)-1)) ## QLIKE from BROWNLESS pg.8 ##(log(x)+(sr.crisis/x))
+                   function(x) (sr.crisis/x-log(sr.crisis/x)-1)) # QLIKE from BROWNLESS pg.8 #(log(x)+(sr.crisis/x))
 mean.ql.rv.mat <- apply(ql.rv.mat, MARGIN = 2, mean)
 mean.ql.sr.mat <- apply(ql.sr.mat, MARGIN = 2, mean)
 mean.ql.rv.mat.1 <- matrix(mean.ql.rv.mat, nrow = 12, ncol = length(roll), byrow = FALSE)
@@ -235,15 +236,15 @@ rownames(ql.final)<- c("ARCH1", "ARCH2", "ARCH3", "ARCH4", "ARCH5",
 mse.final
 ql.final
 
-## Implement Diebold Mariano Test using GARCH(1,1) as baseline model 
-## The null hypothesis is that the two methods have the same forecast accuracy.
-## For alternative="less", the alternative hypothesis is that method 2 is less accurate than method 1. 
-## For alternative="greater", the alternative hypothesis is that method 2 is more accurate than method 1. 
-## For alternative="two.sided", the alternative hypothesis is that method 1 and method 2 have different levels of accuracy.
+# Implement Diebold Mariano Test using GARCH(1,1) as baseline model 
+# The null hypothesis is that the two methods have the same forecast accuracy.
+# For alternative="less", the alternative hypothesis is that method 2 is less accurate than method 1. 
+# For alternative="greater", the alternative hypothesis is that method 2 is more accurate than method 1. 
+# For alternative="two.sided", the alternative hypothesis is that method 1 and method 2 have different levels of accuracy.
 alt.errors <- ql.rv.mat[,c(-11,-23,-35,-47)]
 p.vec <- c()
 test.count <- 1
-alternative <- c("greater") ##, "two.sided", "greater"
+alternative <- c("greater") #, "two.sided", "greater"
 for(m in alternative){
   for(i in 1:ncol(alt.errors)){
     if(i<=11){
@@ -268,7 +269,7 @@ for(m in alternative){
     }
   }
 }
-## Collect all p.values into matrix
+# Collect all p.values into matrix
 p.mat <- matrix(p.vec, nrow = 11, byrow = FALSE)
 rownames(p.mat) <- c("ARCH1", "ARCH2", "ARCH3", "ARCH4", "ARCH5", 
                      "TARCH1", "TARCH2", "TARCH3", "TARCH4", "TARCH5", "TGARCH11")
@@ -276,9 +277,9 @@ colnames(p.mat) <- c(alternative, alternative, alternative, alternative)
 p.mat <- round(p.mat, digits = 4)
 p.mat
 
-## DM Test accross different Rolls (1 week, 2 Week, 3 Week, 4 Week) 1 week as basis
+# DM Test accross different Rolls (1 week, 2 Week, 3 Week, 4 Week) 1 week as basis
 dm.vec <- c()
-alternative <- c("less") ##, "two.sided", "greater"
+alternative <- c("less") #, "two.sided", "greater"
 dm.count <- 1
 for(m in alternative){
   for(i in 1:12){
@@ -301,7 +302,7 @@ dm.mat <- round(dm.mat, digits = 4)
 dm.mat
 
 
-##Plotting 7 day rolling forecasts
+#Plotting 7 day rolling forecasts
 forecast.1 <- as.xts(var.mat[,1:12], order.by = index(crisis.rv))
 names(forecast.1)<- c("ARCH1", "ARCH2", "ARCH3", "ARCH4", "ARCH5", 
                       "TARCH1", "TARCH2", "TARCH3", "TARCH4", "TARCH5", 
@@ -324,18 +325,18 @@ legend("topleft", legend = names(vol.plot),
        lty=1,              
        cex=0.85) 
 
-## MSE tends to choose GARCH11
-## QL tends to chose ARCH1 
+# MSE tends to choose GARCH11
+# QL tends to chose ARCH1 
 
-## Brownless 2.3 Forecast evaluation
-## Our measure of predictive accuracy is based on the average forecast loss 
-## achieved by a model/strategy/proxy triplet. A model that provides a smaller 
-## average loss is more accurate and therefore preferred. 
+# Brownless 2.3 Forecast evaluation
+# Our measure of predictive accuracy is based on the average forecast loss 
+# achieved by a model/strategy/proxy triplet. A model that provides a smaller 
+# average loss is more accurate and therefore preferred. 
 
-##Tsay pg 143 for Forecasting and Equations example 3.1
+#Tsay pg 143 for Forecasting and Equations example 3.1
 
-## GARCH Modeling
-##Engle (1982) and Bollerslev (1986) introduced GARCH models—these account for the volatility clustering that is often seen in the return series of market-priced assets. An example is the popular GARCH(1,1) model:
-##where ht is the variance at time t conditional on past information, and εt is the residual at time t. The three parameters of the model are α, β and ω—there would also generally be a parameter for the mean of the series.
-##Given an estimate of the parameters for a model, it is desirable to determine if the model adequately explains the variance process. A common approach is to divide each residual by the estimated standard deviation for that time point, and square these standardized residuals. Finally perform a Ljung-Box test on the squared standardized residuals (minus their mean). If the statistic is large, then there is evidence that the model is inadequate. Wong and Li (1995) studied the rank Ljung-Box test in this setting.
-##Consider the example of the S&P 500 for dates from 2 January 1985 through 31 December 2001. The return series for this data has 4292 observations. We’ll start with a six-parameter model. The Ljung-Box test statistic with 15 lags for the model is 30.57, giving a p-value of 1%. This is as we expect since the model is known not be very good—it is a GARCH(0,4) model (that is, an ARCH(4) model) assuming a Gaussian distribution for the residuals. This model has four lags of the squared residual and no lags of the conditional variance.
+# GARCH Modeling
+#Engle (1982) and Bollerslev (1986) introduced GARCH models—these account for the volatility clustering that is often seen in the return series of market-priced assets. An example is the popular GARCH(1,1) model:
+#where ht is the variance at time t conditional on past information, and εt is the residual at time t. The three parameters of the model are α, β and ω—there would also generally be a parameter for the mean of the series.
+#Given an estimate of the parameters for a model, it is desirable to determine if the model adequately explains the variance process. A common approach is to divide each residual by the estimated standard deviation for that time point, and square these standardized residuals. Finally perform a Ljung-Box test on the squared standardized residuals (minus their mean). If the statistic is large, then there is evidence that the model is inadequate. Wong and Li (1995) studied the rank Ljung-Box test in this setting.
+#Consider the example of the S&P 500 for dates from 2 January 1985 through 31 December 2001. The return series for this data has 4292 observations. We’ll start with a six-parameter model. The Ljung-Box test statistic with 15 lags for the model is 30.57, giving a p-value of 1%. This is as we expect since the model is known not be very good—it is a GARCH(0,4) model (that is, an ARCH(4) model) assuming a Gaussian distribution for the residuals. This model has four lags of the squared residual and no lags of the conditional variance.
---
+++ README.md
@@ -0,0 +1,6 @@
+Rolling Volatility Forecasts in the Midst of Crisis
+================
+Carsten Stann
+7/5/2019
+
+Volatility permeates the economy. It influences the pricing of assets, options and derivatives as well as portfolio formation. Volatility forecasting, therefore, plays a prominent role in financial decision-making. While it has been shown that one-day ahead volatility forecasts are surprisingly accurate, longer horizon forecasts are more relevant for institutions trading illiquid assets. Generally, models estimated during normal times perform poorly during sudden volatility shocks. In many cases, positions can take days if not weeks to unwind, necessitating quality longer-term volatility forecasts.
